329ce6e8542fae78147661e102c0809dabbf8161
Colette-Liu <colette.liu@gmail.com>
2021-08-27 21:23:40 +1000

    chores includes build drowdown menu for operators

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import React from "react";
+
+const onFirstInputChange = (event) => {
+  console.log("value of firstInput:", event.target.value);
+};
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App" style={{ margin: 10 }}>
+      <form>
+        <input type="number" name="name" onChange={onFirstInputChange} />
+        <select>
+          <option value="grapefruit">+</option>
+          <option value="lime">-</option>
+          <option selected value="coconut">
+            *
+          </option>
+          <option value="mango">/</option>
+        </select>
+        <input type="number" name="name" />
+        <button>=</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
